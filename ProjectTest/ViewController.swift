5a883f9fb8cee18345da80ff54a19b0d7d8f6145
kpunsalan4 <kap113@comcast.net>
2022-06-15 18:57:04 -0400

    I put the art museum links in

--- ProjectTest/ViewController.swift
+++ ProjectTest/ViewController.swift
@@ -18,7 +18,35 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // ART MUSEUM VIRTUAL GALLERY BUTTONS
+    @IBAction func vgButton(_ sender: UIButton) {
+        let vgURL = URL(string:"https://www.vangoghmuseum.nl/en/visit/enjoy-the-museum-from-home")
+        UIApplication.shared.open(vgURL!)
+    }
+    
+    
+    @IBAction func palaceButton(_ sender: UIButton) {
+        let palaceURL = URL(string:"https://tech2.npm.edu.tw/720vr/museumen/views.html")
+        UIApplication.shared.open(palaceURL!)
+    }
+    
+    
+    @IBAction func gugButton(_ sender: UIButton) {
+        let gugURL = URL(string:"https://artsandculture.google.com/project/guggenheim-bilbao")
+        UIApplication.shared.open(gugURL!)
+    }
+    
+    
+    @IBAction func westButton(_ sender: UIButton) {
+        let westURL = URL(string:"https://thewestmoreland.org/visit/virtual-tour/")
+        UIApplication.shared.open(westURL!)
+    }
+    
+    
+    
 
-
+    
+    
 }
 
