9f6239205d9ead87c3bd57dc753a6089ba010ffe
tiffanyw7 <twang45@mtlstudents.net>
2022-06-15 21:46:04 -0400

    created links for music section

--- ProjectTest/MusicViewController.swift
+++ ProjectTest/MusicViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class MusicViewController: UITableViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- ProjectTest/ViewController.swift
+++ ProjectTest/ViewController.swift
@@ -10,9 +10,6 @@ import WebKit
 
 class SecondViewController: UIViewController {
     
-
-
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,11 +39,33 @@ class SecondViewController: UIViewController {
         let westURL = URL(string:"https://thewestmoreland.org/visit/virtual-tour/")
         UIApplication.shared.open(westURL!)
     }
-    
-    
-    
+}
 
+
+    class MusicViewController: UIViewController {
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        @IBAction func musicbutton1(_ sender: UIButton) {
+            let musicURL = URL(string: "https://open.spotify.com/playlist/37i9dQZF1DWWQRwui0ExPn?si=f3d8a3d13c2c4416")
+            UIApplication.shared.open(musicURL!)
     
-    
+        }
+        
+        @IBAction func classical(_ sender: UIButton) {
+            let classicalURL = URL(string: "https://open.spotify.com/playlist/1h0CEZCm6IbFTbxThn6Xcs?si=2fb15665c0cd498f")
+            UIApplication.shared.open(classicalURL!)
+        }
+        
+        @IBAction func nature(_ sender: UIButton) {
+            let natureURL = URL(string: "https://open.spotify.com/playlist/37i9dQZF1DX4PP3DA4J0N8?si=47070d17f982494c")
+            UIApplication.shared.open(natureURL!)
+            
+        }
+        
+        
+
 }
 
